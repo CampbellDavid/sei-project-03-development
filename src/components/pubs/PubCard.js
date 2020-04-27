9a3b1b688eb7ca1ef6e785440979bd74adff3b1f
CampbellDavid <dcampbell092@gmail.com>
2020-04-28 00:14:04 +0100

    dev

--- src/components/pubs/PubCard.js
+++ src/components/pubs/PubCard.js
@@ -1,21 +1,24 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Card, Button } from 'react-bootstrap'
 
 const PubCard = ({ _id, name, image }) => (
-  <>
-    <Link to={`/pubs/${_id}`}>
-      <div className="card">
-        <div className="card-info">
-          <h3>{name}</h3>
-        </div>
-        <div className="colour-header">
-          <figure className="image">
-            <img src={image} alt={name} />
-          </figure>
-        </div>
-      </div>
-    </Link>
-  </>
+	<>
+		<Card className='m-3' bg='dark' style={{ width: '16rem', height: '20rem' }}>
+			<Card.Img style={{ height: '60%' }} variant='top' src={image} />
+			<Card.Body className='font text-light'>{name}</Card.Body>
+			<Card.Footer>
+				<div className='text-center'>
+					<Button
+						className='font text-dark'
+						variant='light'
+						href={`/pubs/${_id}`}
+					>
+						View Pub
+					</Button>
+				</div>
+			</Card.Footer>
+		</Card>
+	</>
 )
 
 export default PubCard
--- src/components/pubs/PubMapComp.js
+++ src/components/pubs/PubMapComp.js
@@ -6,52 +6,52 @@ import Geocoder from 'react-map-gl-geocoder'
 import { Link } from 'react-router-dom'
 
 const PubMapComp = ({
-  viewport,
-  handleGeocoderViewportChange,
-  handleViewportChange,
-  mapboxToken,
-  mapRef,
-  postcodes,
-  pubs
+	viewport,
+	handleGeocoderViewportChange,
+	handleViewportChange,
+	mapboxToken,
+	mapRef,
+	postcodes,
+	pubs,
 }) => {
-  return (
-    <MapGL
-      mapboxApiAccessToken={mapboxToken}
-      ref={mapRef}
-      {...viewport}
-      height={'100vh'}
-      width={'100vw'}
-      mapStyle="mapbox://styles/mapbox/streets-v11"
-      onViewportChange={handleViewportChange}
-    >
-      <Geocoder
-        mapRef={mapRef}
-        onViewportChange={handleGeocoderViewportChange}
-        mapboxApiAccessToken={mapboxToken}
-      />
-      {postcodes.map((postcode, index) => {
-        return (
-          <Marker
-            key={index}
-            latitude={postcode.result.latitude}
-            longitude={postcode.result.longitude}
-          >
-            <button className="marker">
-              {pubs.map((pub, i) => {
-                return pub.postcode === postcode.query ? (
-                  <Link key={i} to={`/pubs/${pub._id}`}>
-                    <div>
-                      <img src={pub.image} />
-                    </div>
-                  </Link>
-                ) : null
-              })}
-            </button>
-          </Marker>
-        )
-      })}
-    </MapGL>
-  )
+	return (
+		<MapGL
+			mapboxApiAccessToken={mapboxToken}
+			ref={mapRef}
+			{...viewport}
+			height={'100vh'}
+			width={'100vw'}
+			mapStyle='mapbox://styles/mapbox/streets-v11'
+			onViewportChange={handleViewportChange}
+		>
+			<Geocoder
+				mapRef={mapRef}
+				onViewportChange={handleGeocoderViewportChange}
+				mapboxApiAccessToken={mapboxToken}
+			/>
+			{postcodes.map((postcode, index) => {
+				return (
+					<Marker
+						key={index}
+						latitude={postcode.result.latitude}
+						longitude={postcode.result.longitude}
+					>
+						<button className='marker'>
+							{pubs.map((pub, i) => {
+								return pub.postcode === postcode.query ? (
+									<Link key={i} to={`/pubs/${pub._id}`}>
+										<div>
+											<img src={pub.image} />
+										</div>
+									</Link>
+								) : null
+							})}
+						</button>
+					</Marker>
+				)
+			})}
+		</MapGL>
+	)
 }
 
 export default PubMapComp
