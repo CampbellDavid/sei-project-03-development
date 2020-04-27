da8cfaf740234c52871d1432b71d483ca6f572d6
CampbellDavid <dcampbell092@gmail.com>
2020-04-28 00:47:21 +0100

    dev

--- src/components/teams/TeamNew.js
+++ src/components/teams/TeamNew.js
@@ -4,38 +4,45 @@ import Authorization from '../../../lib/authorization'
 import TeamForm from './TeamForm'
 
 export default class TeamNew extends React.Component {
-  state = {
-    team: {
-      teamName: ''
-    }
-  }
+	state = {
+		team: {
+			teamName: '',
+		},
+	}
 
-  handleChange = ({ target: { name, value } }) => {
-    const team = { ...this.state.team, [name]: value }
-    this.setState({ team })
-  }
+	handleChange = ({ target: { name, value } }) => {
+		const team = { ...this.state.team, [name]: value }
+		this.setState({ team })
+	}
 
-  handleSubmit = async e => {
-    e.preventDefault()
-    const eventId = this.props.match.params.id
-    try {
-      await axios.post(`/api/events/${eventId}/teams`, this.state.team, {
-        headers: { Authorization: `Bearer ${Authorization.getToken()}` }
-      })
-      this.props.history.push(`/events/${eventId}`)
-    } catch (error) {
-      console.log(error)
-    }
-  }
+	handleSubmit = async (e) => {
+		e.preventDefault()
+		const eventId = this.props.match.params.id
+		try {
+			await axios.post(`/api/events/${eventId}/teams`, this.state.team, {
+				headers: { Authorization: `Bearer ${Authorization.getToken()}` },
+			})
+			this.props.history.push(`/events/${eventId}`)
+		} catch (error) {
+			console.log(error)
+		}
+	}
 
-  render() {
-    return (
-      <>
-        <TeamForm
-          team={this.state.team}
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit} />
-      </>
-    )
-  }
+	render() {
+		return (
+			<div className='bg-image'>
+				<div className='bg-fade center-x center-y'>
+					<div className='body-div'>
+						<section className='form text-center mt-5 mb-5'>
+							<TeamForm
+								team={this.state.team}
+								handleChange={this.handleChange}
+								handleSubmit={this.handleSubmit}
+							/>
+						</section>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
