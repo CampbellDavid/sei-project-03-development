ac48514ab8f82ccfda235ca8000767eb7a32d41a
CampbellDavid <dcampbell092@gmail.com>
2020-04-28 00:23:53 +0100

    dev

--- src/components/pubs/PubForm.js
+++ src/components/pubs/PubForm.js
@@ -2,122 +2,136 @@ import React from 'react'
 
 import ImageUpload from '../common/ImageUpload'
 
-
 const PubForm = ({ data, handleChange, handleSubmit }) => {
-  return (
-    <section className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="form-div">
-          <input
-            onChange={handleChange}
-            placeholder="Pub Name"
-            name="name" 
-            value={data.name}
-            required/>
-        </div>
-        <div className="form-div">
-          <input
-            onChange={handleChange}
-            placeholder="Street Address"
-            name="streetName" 
-            value={data.streetName}
-            required/>
-        </div>
-        <div className="form-div">
-          <input
-            onChange={handleChange}
-            placeholder="City"
-            name="city" 
-            value={data.city}
-            required/>
-        </div>
-        <div className="form-div">
-          <input
-            onChange={handleChange}
-            placeholder="Postcode"
-            name="postcode" 
-            value={data.postcode}
-            required/>
-        </div>
-        <div className="form-div">
-          <input
-            onChange={handleChange}
-            placeholder="Phone Number"
-            name="phone" 
-            value={data.phone}
-          />
-        </div>
-        <div className="form-div">
-          <input
-            onChange={handleChange}
-            placeholder="Website"
-            name="website" 
-            value={data.website}
-          />
-        </div>
-        <div className="form-div">
-          <input
-            onChange={handleChange}
-            placeholder="Pub Description"
-            name="description" 
-            value={data.description}
-          />
-        </div>
-        <div className="form-div">
-          <input
-            type="number"
-            onChange={handleChange}
-            placeholder="Max. Team Size"
-            name="maxTeamSize" 
-            value={data.maxTeamSize}
-            required/>
-        </div>
-        <div className="form-div">
-          <input
-            onChange={handleChange}
-            placeholder="Quiz Day"
-            name="quizDay" 
-            value={data.quizDay}
-            required/>
-        </div>
-        <div className="form-div">
-          <input
-            type="time"
-            onChange={handleChange}
-            placeholder="Quiz Time"
-            name="quizTime" 
-            value={data.quizTime}
-            required/>
-        </div>
-        <div className="form-div">
-          <input
-            onChange={handleChange}
-            placeholder="Average Pint Cost"
-            name="averagePintCost" 
-            value={data.averagePintCost}
-            required/>
-        </div>
-        <div className="form-div">
-          <label>* required field</label>
-          <ImageUpload
-            labelText="Upload Image"
-            handleChange={handleChange}
-            fieldName="image"
-            required/>
-        
-        </div>
-        <div className="button-div">
-          <button
-            className="button"
-            type="submit">
-              Submit</button>
-        </div>
-      </form>
-    </section>
-  )
+	return (
+		<div>
+			<h1 className='font text-1 mb-3'>Create a new pub/edit pub details</h1>
+			<form onSubmit={handleSubmit} className='text-center'>
+				<div>
+					<input
+						className='p-2 m-2 font no-line width-80 rounded'
+						onChange={handleChange}
+						placeholder='Pub Name'
+						name='name'
+						value={data.name}
+						required
+					/>
+				</div>
+				<div>
+					<input
+						className='p-2 m-2 font no-line width-80 rounded'
+						onChange={handleChange}
+						placeholder='Street Address'
+						name='streetName'
+						value={data.streetName}
+						required
+					/>
+				</div>
+				<div>
+					<input
+						className='p-2 m-2 font no-line width-80 rounded'
+						onChange={handleChange}
+						placeholder='City'
+						name='city'
+						value={data.city}
+						required
+					/>
+				</div>
+				<div>
+					<input
+						className='p-2 m-2 font no-line width-80 rounded'
+						onChange={handleChange}
+						placeholder='Postcode'
+						name='postcode'
+						value={data.postcode}
+						required
+					/>
+				</div>
+				<div>
+					<input
+						className='p-2 m-2 font no-line width-80 rounded'
+						onChange={handleChange}
+						placeholder='Phone Number'
+						name='phone'
+						value={data.phone}
+					/>
+				</div>
+				<div>
+					<input
+						className='p-2 m-2 font no-line width-80 rounded'
+						onChange={handleChange}
+						placeholder='Website'
+						name='website'
+						value={data.website}
+					/>
+				</div>
+				<div>
+					<input
+						className='p-2 m-2 font no-line width-80 rounded'
+						onChange={handleChange}
+						placeholder='Pub Description'
+						name='description'
+						value={data.description}
+					/>
+				</div>
+				<div>
+					<input
+						className='p-2 m-2 font no-line width-80 rounded'
+						type='number'
+						onChange={handleChange}
+						placeholder='Max. Team Size'
+						name='maxTeamSize'
+						value={data.maxTeamSize}
+						required
+					/>
+				</div>
+				<div>
+					<input
+						className='p-2 m-2 font no-line width-80 rounded'
+						onChange={handleChange}
+						placeholder='Quiz Day'
+						name='quizDay'
+						value={data.quizDay}
+						required
+					/>
+				</div>
+				<div>
+					<input
+						className='p-2 m-2 font no-line width-80 rounded'
+						type='time'
+						onChange={handleChange}
+						placeholder='Quiz Time'
+						name='quizTime'
+						value={data.quizTime}
+						required
+					/>
+				</div>
+				<div>
+					<input
+						className='p-2 m-2 font no-line width-80 rounded'
+						onChange={handleChange}
+						placeholder='Average Pint Cost'
+						name='averagePintCost'
+						value={data.averagePintCost}
+						required
+					/>
+				</div>
+				<div>
+					<ImageUpload
+						labelText='Upload Image'
+						handleChange={handleChange}
+						fieldName='image'
+						required
+					/>
+				</div>
+				<div className='button-div'>
+					<button className='m-2 font btn btn-dark' type='submit'>
+						Submit
+					</button>
+				</div>
+			</form>
+		</div>
+	)
 }
 
 export default PubForm
-
-
-
--- src/components/pubs/PubNew.js
+++ src/components/pubs/PubNew.js
@@ -5,49 +5,55 @@ import Authorization from '../../../lib/authorization'
 import PubForm from './PubForm'
 
 export default class PubNew extends React.Component {
-  state = {
-    data: {
-      name: '',
-      image: '',
-      city: '',
-      streetName: '',
-      postcode: '',
-      phone: '',
-      website: '',
-      description: '',
-      maxTeamSize: '',
-      quizDay: '',
-      quizTime: '',
-      averagePintCost: ''
-    }
-  }
+	state = {
+		data: {
+			name: '',
+			image: '',
+			city: '',
+			streetName: '',
+			postcode: '',
+			phone: '',
+			website: '',
+			description: '',
+			maxTeamSize: '',
+			quizDay: '',
+			quizTime: '',
+			averagePintCost: '',
+		},
+	}
 
-  handleChange = ({ target: { name, value } }) => {
-    const data = { ...this.state.data, [name]: value }
-    this.setState({ data })
-  }
+	handleChange = ({ target: { name, value } }) => {
+		const data = { ...this.state.data, [name]: value }
+		this.setState({ data })
+	}
 
-  handleSubmit = async e => {
-    e.preventDefault()
-    try {
-      const res = await axios.post('/api/pubs', this.state.data, {
-        headers: { Authorization: `Bearer ${Authorization.getToken()}` }
-      })
-      this.props.history.push(`/pubs/${res.data._id}`)
-    } catch (error) {
-      console.log(error)
-    }
-  }
+	handleSubmit = async (e) => {
+		e.preventDefault()
+		try {
+			const res = await axios.post('/api/pubs', this.state.data, {
+				headers: { Authorization: `Bearer ${Authorization.getToken()}` },
+			})
+			this.props.history.push(`/pubs/${res.data._id}`)
+		} catch (error) {
+			console.log(error)
+		}
+	}
 
-  render() {
-    return (
-      <>
-        <h1>Create a new pub!</h1>
-        <PubForm
-          data={this.state.data}
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit} />
-      </>
-    )
-  }
+	render() {
+		return (
+			<div className='bg-image'>
+				<div className='bg-fade center-x center-y'>
+					<div className='body-div'>
+						<section className='form text-center mt-5 mb-5'>
+							<PubForm
+								data={this.state.data}
+								handleChange={this.handleChange}
+								handleSubmit={this.handleSubmit}
+							/>
+						</section>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
