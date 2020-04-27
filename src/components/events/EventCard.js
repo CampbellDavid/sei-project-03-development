9e48f853ec90b4b8d3f42d9b19f3e6a49f4ed3a9
CampbellDavid <dcampbell092@gmail.com>
2020-04-28 00:16:11 +0100

    dev

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -5,7 +5,7 @@ import Image from '../../assets/images/wine-glass.png'
 
 const EventCard = ({ pub, entryFee, quizDay, quizTime, _id }) => (
 	<Card className='m-3' bg='dark' style={{ width: '16rem' }}>
-		<Card.Header className='font text-light'>{pub}</Card.Header>
+		<Card.Header className='font text-light bg-secondary'>{pub}</Card.Header>
 		<Card.Body>
 			<Card.Title className='font text-light'>{entryFee}</Card.Title>
 			<Card.Text className='font text-light'>
