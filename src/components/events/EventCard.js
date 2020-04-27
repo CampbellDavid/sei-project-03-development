2d26f1e911de3ef4df7af2c9b69ca7dcab014aa0
CampbellDavid <dcampbell092@gmail.com>
2020-04-27 20:10:40 +0100

    dev

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Card, Button } from 'react-bootstrap'
+import Image from '../../assets/images/wine-glass.png'
 
 const EventCard = ({ pub, entryFee, quizDay, quizTime, _id }) => (
-  <>
-    <Link to={`/events/${_id}`}>
-      <div className="card">
-        <div className="card-info">
-          <h2>{pub}</h2>
-          <p>{entryFee} | {quizDay} | {quizTime}</p>
-        </div>
-      </div>
-    </Link>
-  </>
+	<Card className='m-3' style={{ width: '18rem' }}>
+		{/* <Card.Img style={{ height: '50%' }} variant='top' src={Image} /> */}
+		<Card.Body className='d-flex flex-column'>
+			<Card.Title>{pub}</Card.Title>
+
+			<div className='mt-auto'>
+				<Card.Subtitle className='pt-2 pb-2'>{entryFee}</Card.Subtitle>
+				<Card.Subtitle className='pt-2 pb-2'>
+					{quizDay} | {quizTime}
+				</Card.Subtitle>
+				<div className='center-item-screen'>
+					<Button variant='dark' href={`/events/${_id}`}>
+						View Event
+					</Button>
+				</div>
+			</div>
+		</Card.Body>
+	</Card>
 )
 export default EventCard
--- src/components/events/EventIndex.js
+++ src/components/events/EventIndex.js
@@ -8,100 +8,112 @@ import EventCard from './EventCard'
 import Authorization from '../../../lib/authorization'
 
 export default class EventIndex extends React.Component {
-  state = {
-    postcodes: null,
-    events: null,
-    viewport: {
-      latitude: 51.5074,
-      longitude: 0.1278,
-      zoom: 9
-    }
-  }
+	state = {
+		postcodes: null,
+		events: null,
+		viewport: {
+			latitude: 51.5074,
+			longitude: 0.1278,
+			zoom: 9,
+		},
+	}
 
-  mapboxToken = process.env.MAPBOX_ACCESS_TOKEN;
-  mapRef = React.createRef()
+	mapboxToken = process.env.MAPBOX_ACCESS_TOKEN
+	mapRef = React.createRef()
 
-  handleViewportChange = viewport => {
-    this.setState({
-      viewport: { ...this.state.viewport, ...viewport }
-    })
-  }
+	handleViewportChange = (viewport) => {
+		this.setState({
+			viewport: { ...this.state.viewport, ...viewport },
+		})
+	}
 
-  handleGeocoderViewportChange = viewport => {
-    const geocoderDefaultOverrides = { transitionDuration: 1000 }
-    this.setState({
-      viewport: {
-        latitude: viewport.latitude,
-        longitude: viewport.longitude,
-        zoom: viewport.zoom
-      }
-    })
-    return this.handleViewportChange({
-      ...viewport,
-      ...geocoderDefaultOverrides
-    })
-  };
+	handleGeocoderViewportChange = (viewport) => {
+		const geocoderDefaultOverrides = { transitionDuration: 1000 }
+		this.setState({
+			viewport: {
+				latitude: viewport.latitude,
+				longitude: viewport.longitude,
+				zoom: viewport.zoom,
+			},
+		})
+		return this.handleViewportChange({
+			...viewport,
+			...geocoderDefaultOverrides,
+		})
+	}
 
-  async componentDidMount() {
-    try {
-      const res = await axios.get('/api/events')
-      console.log('data', res.data)
-      this.setState({ events: res.data })
-      this.getPostcodes()
-    } catch (err) {
-      console.log(err)
-    }
-  }
+	async componentDidMount() {
+		try {
+			const res = await axios.get('/api/events')
+			console.log('data', res.data)
+			this.setState({ events: res.data })
+			this.getPostcodes()
+		} catch (err) {
+			console.log(err)
+		}
+	}
 
-  async getPostcodes() {
-    const postcodes = this.state.events.map(event => {
-      return event.postcode
-    })
-    const res = await axios.post(
-      'https://cors-anywhere.herokuapp.com/api.postcodes.io/postcodes',
-      { postcodes }
-    )
-    this.setState({ postcodes: res.data.result })
-  }
+	async getPostcodes() {
+		const postcodes = this.state.events.map((event) => {
+			return event.postcode
+		})
+		const res = await axios.post(
+			'https://cors-anywhere.herokuapp.com/api.postcodes.io/postcodes',
+			{ postcodes }
+		)
+		this.setState({ postcodes: res.data.result })
+	}
 
-  render() {
-    if (!this.state.events) return null
-    if (!this.state.postcodes) return null
-    return (
-      <section>
-        <div className="title">
-          <h1>Find an event near you!</h1>
-        </div>
+	render() {
+		if (!this.state.events) return null
+		if (!this.state.postcodes) return null
+		return (
+			<div className='bg-image'>
+				<div className='bg-fade'>
+					<div className='body-div'>
+						<section>
+							<div className='mt-3'>
+								<h1 className='text-center font text-1-white'>
+									Find an event near you
+								</h1>
+							</div>
 
-        <div className="index">
-          <div className="card-container">
-            {this.state.events.map(event => (
-              <EventCard key={event._id} {...event} />
-            ))}
+							<div>
+								<div>
+									<div className='row'>
+										<div className='center-x'>
+											{this.state.events.map((event) => (
+												<EventCard key={event._id} {...event} />
+											))}
+										</div>
+									</div>
+									{Authorization.isAuthenticated() ? (
+										<Link to='/events/new'>
+											<button type='button' className='button'>
+												New Event
+											</button>
+										</Link>
+									) : null}
+								</div>
 
-
-            {Authorization.isAuthenticated() ?
-              <Link to="/events/new">
-                <button type="button"
-                  className="button">New Event</button>
-              </Link>
-              : null}
-          </div>
-
-
-          <div className="map-container">
-            <EventMapComp
-              viewport={this.state.viewport}
-              handleGeocoderViewportChange={this.handleGeocoderViewportChange}
-              handleViewportChange={this.handleViewportChange}
-              mapboxToken={this.mapboxToken}
-              mapRef={this.mapRef}
-              postcodes={this.state.postcodes}
-              events={this.state.events}
-            />
-          </div>
-        </div>
-      </section>
-    )
-  }
+								<div className='map-container'>
+									<EventMapComp
+										viewport={this.state.viewport}
+										handleGeocoderViewportChange={
+											this.handleGeocoderViewportChange
+										}
+										handleViewportChange={this.handleViewportChange}
+										mapboxToken={this.mapboxToken}
+										mapRef={this.mapRef}
+										postcodes={this.state.postcodes}
+										events={this.state.events}
+									/>
+								</div>
+							</div>
+						</section>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
