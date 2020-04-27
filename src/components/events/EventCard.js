9f8caa08e8cd23a2f478ee8a0e332af7698f5826
CampbellDavid <dcampbell092@gmail.com>
2020-04-27 22:13:36 +0100

    dev

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -4,23 +4,26 @@ import { Card, Button } from 'react-bootstrap'
 import Image from '../../assets/images/wine-glass.png'
 
 const EventCard = ({ pub, entryFee, quizDay, quizTime, _id }) => (
-	<Card className='m-3' style={{ width: '18rem' }}>
-		{/* <Card.Img style={{ height: '50%' }} variant='top' src={Image} /> */}
-		<Card.Body className='d-flex flex-column'>
-			<Card.Title>{pub}</Card.Title>
-
-			<div className='mt-auto'>
-				<Card.Subtitle className='pt-2 pb-2'>{entryFee}</Card.Subtitle>
-				<Card.Subtitle className='pt-2 pb-2'>
-					{quizDay} | {quizTime}
-				</Card.Subtitle>
-				<div className='center-item-screen'>
-					<Button variant='dark' href={`/events/${_id}`}>
-						View Event
-					</Button>
-				</div>
-			</div>
+	<Card bg='secondary' style={{ width: '18rem' }}>
+		<Card.Header className='font text-light'>{pub}</Card.Header>
+		<Card.Body>
+			<Card.Title className='font text-light'>{entryFee}</Card.Title>
+			<Card.Text className='font text-light'>
+				{quizDay} | {quizTime}
+			</Card.Text>
 		</Card.Body>
+		<Card.Footer>
+			{' '}
+			<div className='text-center'>
+				<Button
+					className='font text-light'
+					variant='dark'
+					href={`/events/${_id}`}
+				>
+					View Event
+				</Button>
+			</div>
+		</Card.Footer>
 	</Card>
 )
 export default EventCard
