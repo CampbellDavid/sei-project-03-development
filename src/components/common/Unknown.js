d17b75d833b4ac61bf6d56938b199acee870ad7b
CampbellDavid <dcampbell092@gmail.com>
2020-04-29 17:46:32 +0100

    dev

--- src/components/common/Unknown.js
+++ src/components/common/Unknown.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const Unknown = () => {
-  return (
-    <h1 className="unknown-route">404: Page Not Found</h1>
-  )
+	return (
+		<div className='bg-image'>
+			<div className='bg-fade-high'>
+				<div className='body-div'>
+					<section style={{ overflowX: 'hidden' }}>
+						<div className='m-3'>
+							<h1 className='text-center font text-1'>
+								Error 404: Page does not exist
+							</h1>
+						</div>
+					</section>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Unknown
